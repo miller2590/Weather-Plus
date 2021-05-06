695491759b17a38453a52641d0f4306e34e12cab
miller2590 <59415200+miller2590@users.noreply.github.com>
2021-05-06 17:23:29 -0500

    created config.py to clean main app.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from configparser import ConfigParser
+
+# Configuration
+config_file = 'config.ini'
+config = ConfigParser()
+config.read(config_file)
+api_key = config['gfg']['api']
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid={}'
